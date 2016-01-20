e7da94d200f9b0c14ad02df27d5ba81b9d1ff9e3
mphonq <mpho.nqache@gmail.com>
2016-01-20 09:56:31 +0100

    Current version can connect to an IOSXRv device, verify the existence and of the specified ACL, as well the individual lines within the ACL, it also checks if the ACL has been applied in hardware. The script is able to remove a specified line from the ACL and replace it with a new one, and do a post verification. Still to be done is to read specified ACLś from a file, error handlind for being unable to connect to a device, or if an ACL doesn´t exist and other error handling functionality.

---
+++ ACL-Scripts/Deny_end_of_line.py
@@ -0,0 +1,78 @@
+import pexpect
+import sys
+from tabnanny import verbose
+
+
+
+
+def connect():
+    host = raw_input("Please enter the host ip: ")
+    user = raw_input("Please enter your username: ")
+    password = raw_input("Please enter your password: ")
+    child = pexpect.spawn('ssh %s@%s' % (user, host))
+    child.timeout = 4
+    child.expect('password:')
+    child.sendline(password)
+    child.expect('#')
+    return child
+
+            #child.sendline('configure exclusive')
+            #child.expect('\(config)\)#')
+
+def preCheck(child):
+
+    child.sendline('terminal length 0')
+    child.expect('#')
+    child.sendline('show ipv4 access-list v4-asd-PB400-LRD-179-179-out')
+    child.expect('#')
+    print child.before
+    child.sendline('\r\n')
+    print("\n\n")
+    child.sendline('show access-list v4-asd-PB400-LRD-179-179-out hardware egress location 0/0/CPU0')
+    child.expect('#')
+    print child.before
+    child.sendline('\r\n')
+
+
+
+def removeAndReplaceLine(child):
+    removeLine = raw_input("Please enter the line you would like to remove and replace: ")
+    int(removeLine)
+    child.sendline('configure exclusive')
+    child.expect('.*#.*')
+    child.sendline('ipv4 access-list v4-asd-PB400-LRD-179-179-out')
+    child.expect('.*#.*')
+    child.sendline('no ' + removeLine)
+    child.expect('.*#.*')
+    child.sendline('100000 deny ipv4 any any log')
+    child.expect('.*#.*')
+    child.sendline('show conf')
+    child.expect('.*#.*')
+    print child.before
+    forward = raw_input("Do you want to commit: ")
+    forward.lower()
+    if forward == "yes":
+        child.expect('.*#.*')
+        child.sendline('commit')
+    else:
+        child.sendline('abort')
+        print("operation aborted")
+        sys.exit(0)
+
+
+def postCheck(child):
+    child.sendline('do show ipv4 access-list v4-asd-PB400-LRD-179-179-out')
+    child.expect('.*#.*')
+    print child.before
+    print("\n\n\n")
+    child.sendline('do show access-list v4-asd-PB400-LRD-179-179-out hardware egress location 0/0/CPU0')
+    child.expect('.*#.*')
+    print child.before
+
+
+def main():
+    child = connect()
+    preCheck(child)
+    removeAndReplaceLine(child)
+    postCheck(child)
+main()
