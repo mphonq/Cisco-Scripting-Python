68a501680b870d38bdb05f9ba8b1f01b8eaf254f
mphonq <mpho.nqache@gmail.com>
2016-02-10 12:03:24 +0100

    Current version can connect to an IOSXRv device, verify the existence and of the specified ACL, as well the individual lines within the ACL, it also checks if the ACL has been applied in hardware. The script is able to remove a specified line from the ACL and replace it with a new one, and do a post verification. Still to be done is to read specified ACLś from a file, error handlind for being unable to connect to a device, or if an ACL doesn´t exist and other error handling functionality.

--- ACL-Scripts/Deny_end_of_line.py
+++ ACL-Scripts/Deny_end_of_line.py
@@ -1,10 +1,20 @@
 import pexpect
 import sys
+import re
 from tabnanny import verbose
 
 
-access_list = raw_input("Please enter the name of the ACL to be modified: ")
+def readFile():
+    try:
+        aclList = open("acl.txt", 'r').read().split()
 
+    except IOError as e:
+        print "I/O error({0}): {1}".format(e.errno, e.strerror)
+
+    return aclList
+
+
+"""
 def connect():
     host = raw_input("Please enter the host ip: ")
     user = raw_input("Please enter your username: ")
@@ -15,11 +25,44 @@ def connect():
     child.sendline(password)
     child.expect('#')
     return child
+"""
 
-            #child.sendline('configure exclusive')
-            #child.expect('\(config)\)#')
 
-def preCheck(child):
+def determineIPVersion(line):
+    counter = len(line)
+    for i in range(0, counter):
+        access_list = line[i]
+        search_ipv4 = (re.search(r'v4', access_list))
+        search_ipv6 = (re.search(r'v6', access_list))
+        if search_ipv4:
+            ip_ver = "v4"
+            return ip_ver
+        elif search_ipv6:
+            ip_ver = "v6"
+            return ip_ver
+        else:
+            print "Could not detect ip version."
+            sys.exit(0)
+
+def determineDirection(line):
+    counter = len(line)
+    for i in range(0, counter):
+        access_list = line[i]
+        search_ingress = (re.search(r'in', access_list))
+        search_egress = (re.search(r'out', access_list))
+        if search_egress:
+            direction = "egress"
+            return direction
+        elif search_ingress:
+            direction = "ingress"
+            return direction
+        else:
+            print "could not determine direction."
+            sys.exit(0)
+    """
+    for i in range(len(access_list)):
+        current_ACL = access_list[i]
+        print current_ACL
 
     child.sendline('terminal length 0')
     child.expect('#')
@@ -28,15 +71,14 @@ def preCheck(child):
     print child.before
     child.sendline('\r\n')
     print("\n\n")
-    hardware_direction = raw_input("Please enter the direction of the ACL in hardware: ")
-    child.sendline('show access-list ' + access_list + 'hardware '+ hardware_direction + 'location 0/0/CPU0')
+    child.sendline('show access-list ' + access_list + 'hardware '+ direction + 'location 0/0/CPU0')
     child.expect('#')
     print child.before
     child.sendline('\r\n')
 
 
 
-def removeAndReplaceLine(child):
+def removeAndReplaceLine(child, access_list):
     removeLine = raw_input("Please enter the line you would like to remove and replace: ")
     int(removeLine)
     child.sendline('configure exclusive')
@@ -50,24 +92,30 @@ def removeAndReplaceLine(child):
     child.sendline('show conf')
     child.expect('.*#.*')
     print child.before
-    forward = raw_input("Do you want to commit: ")
-    forward.lower()
-    if forward == "yes":
+    commit = raw_input("Do you want to commit: ")
+    commit.lower()
+    if commit == "yes":
         child.expect('.*#.*')
         child.sendline('commit')
     else:
         child.sendline('abort')
         print("operation aborted")
         sys.exit(0)
-
+    child.sendline('exit\n')
 
 def postCheck(child):
     preCheck(child)
 
+"""
+
 
 def main():
-    child = connect()
-    preCheck(child)
-    removeAndReplaceLine(child)
-    postCheck(child)
+    line = readFile()
+    # child = connect()
+    determineIPVersion(line)
+    determineDirection(line)
+    # removeAndReplaceLine(child, line)
+    # postCheck(child)
+
+
 main()
