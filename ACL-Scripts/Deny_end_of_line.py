4730f471a919fa7324c5bccf91d9c499520a49be
mphonq <mpho.nqache@gmail.com>
2016-02-11 11:29:13 +0100

    Current version can connect to an IOSXRv device, verify the existence and of the specified ACL, as well the individual lines within the ACL, it also checks if the ACL has been applied in hardware. The script is able to remove a specified line from the ACL and replace it with a new one, and do a post verification. Still to be done is to read specified ACLś from a file, error handlind for being unable to connect to a device, or if an ACL doesn´t exist and other error handling functionality.

--- ACL-Scripts/Deny_end_of_line.py
+++ ACL-Scripts/Deny_end_of_line.py
@@ -1,9 +1,10 @@
 import pexpect
 import sys
 import re
+import time
 from tabnanny import verbose
 
-
+"""
 def readFile():
     try:
         aclList = open("acl.txt", 'r').read().split()
@@ -13,25 +14,25 @@ def readFile():
 
     return aclList
 
-
 """
+
 def connect():
     host = raw_input("Please enter the host ip: ")
     user = raw_input("Please enter your username: ")
     password = raw_input("Please enter your password: ")
-    child = pexpect.spawn('ssh %s@%s' % (user, host))
-    child.timeout = 4
-    child.expect('password:')
+    child = pexpect.spawn('telnet ' + host)
+    fout = file('mylog.txt','w')
+    child.logfile = fout
+    child.expect('Username: ')
+    child.sendline(user)
+    child.expect('Password: ')
     child.sendline(password)
+    child.timeout = 4
     child.expect('#')
     return child
-"""
 
 
-def determineIPVersion(line):
-    counter = len(line)
-    for i in range(0, counter):
-        access_list = line[i]
+def determineIPVersion(access_list):
         search_ipv4 = (re.search(r'v4', access_list))
         search_ipv6 = (re.search(r'v6', access_list))
         if search_ipv4:
@@ -44,10 +45,7 @@ def determineIPVersion(line):
             print "Could not detect ip version."
             sys.exit(0)
 
-def determineDirection(line):
-    counter = len(line)
-    for i in range(0, counter):
-        access_list = line[i]
+def determineDirection(access_list):
         search_ingress = (re.search(r'in', access_list))
         search_egress = (re.search(r'out', access_list))
         if search_egress:
@@ -57,65 +55,111 @@ def determineDirection(line):
             direction = "ingress"
             return direction
         else:
-            print "could not determine direction."
+            print "Could not determine hardware direction."
             sys.exit(0)
-    """
-    for i in range(len(access_list)):
-        current_ACL = access_list[i]
-        print current_ACL
 
-    child.sendline('terminal length 0')
-    child.expect('#')
-    child.sendline('show ipv4 ' + access_list)
-    child.expect('#')
-    print child.before
-    child.sendline('\r\n')
-    print("\n\n")
-    child.sendline('show access-list ' + access_list + 'hardware '+ direction + 'location 0/0/CPU0')
-    child.expect('#')
-    print child.before
-    child.sendline('\r\n')
+def preCheck(child, access_list, ip_ver, direction):
+    if ip_ver == 'v4':
+        child.sendline('terminal length 0')
+        child.expect('#')
+        child.sendline('show access-lists ipv4 ' + access_list)
+        child.expect('#')
+        print child.before
+        child.sendline('\r\n')
+        print("\n\n")
+        child.sendline('show access-list ' + access_list + 'hardware '+ direction + 'location 0/0/CPU0')
+        child.expect('#')
+        print child.before
+        child.sendline('\r\n')
+    elif ip_ver == 'v6':
+        child.sendline('terminal length 0')
+        child.expect('#')
+        child.sendline('show access-lists ipv6 ' + access_list)
+        child.expect('#')
+        print child.before
+        child.sendline('\r\n')
+        print("\n\n")
+        child.sendline('show access-list ipv6' + access_list + 'hardware '+ direction + 'location 0/0/CPU0')
+        child.expect('#')
+        print child.before
+        child.sendline('\r\n')
 
 
 
-def removeAndReplaceLine(child, access_list):
+def removeAndReplaceLine(child, access_list, ip_ver):
     removeLine = raw_input("Please enter the line you would like to remove and replace: ")
     int(removeLine)
     child.sendline('configure exclusive')
     child.expect('.*#.*')
-    child.sendline('ipv4 access-list ' + access_list)
-    child.expect('.*#.*')
-    child.sendline('no ' + removeLine)
-    child.expect('.*#.*')
-    child.sendline('100000 deny ipv4 any any log')
-    child.expect('.*#.*')
-    child.sendline('show conf')
-    child.expect('.*#.*')
-    print child.before
-    commit = raw_input("Do you want to commit: ")
-    commit.lower()
-    if commit == "yes":
+    if ip_ver == 'v4':
+        child.sendline('ipv4 access-list ' + access_list)
         child.expect('.*#.*')
-        child.sendline('commit')
-    else:
-        child.sendline('abort')
-        print("operation aborted")
-        sys.exit(0)
-    child.sendline('exit\n')
+        child.sendline('no ' + removeLine)
+        child.expect('.*#.*')
+        child.sendline('100000 deny ipv4 any any log')
+        child.expect('.*#.*')
+        print "This is a confirmation of the line you would like to remove and replace.\n"
+        child.sendline('show conf')
+        time.sleep(2)
+        print child.after
+        commit = raw_input("Do you want to commit: ")
+        commit.lower()
+        if commit == "yes":
+            child.sendline('commit')
+            child.expect('.*#.*')
+        else:
+            child.sendline('abort')
+            print("operation aborted")
+            sys.exit(0)
+    elif ip_ver == 'v6':
+        child.sendline('ipv6 access-list ' + access_list)
+        child.expect('.*#.*')
+        child.sendline('no ' + removeLine)
+        child.expect('.*#.*')
+        child.sendline('100000 deny ipv6 any any log')
+        child.expect('.*#.*')
+        print "This is a confirmation of the line you would like to remove and replace.\n"
+        child.sendline('show conf')
+        child.expect('.*#.*')
+        time.sleep(4)
 
-def postCheck(child):
-    preCheck(child)
+        child.expect('.*#.*')
+        print child.after
+        ipv6Commit = raw_input("Do you want to commit: ")
+        ipv6Commit.lower()
+        if ipv6Commit == "yes":
+            child.sendline('commit')
+            child.expect('.*#.*')
+            child.sendline('exit')
+            child.expect('.*#.*')
+            child.sendline('exit')
 
-"""
-
+        else:
+            child.sendline('abort')
+            print("operation aborted")
+            sys.exit(0)
 
 def main():
-    line = readFile()
-    # child = connect()
-    determineIPVersion(line)
-    determineDirection(line)
-    # removeAndReplaceLine(child, line)
-    # postCheck(child)
-
+    line = raw_input("Please enter the acl you would like to edit: ")
+    child = connect()
+    ip_ver = determineIPVersion(line)
+    direction = determineDirection(line)
+    preCheck(child, line, ip_ver, direction)
+    removeAndReplaceLine(child, line, ip_ver)
+    preCheck(child, line, ip_ver, direction)
+    more = raw_input("do you have another acl you would like to edit: ")
+    more.lower()
+    while more == "yes":
+        sameDevice = raw_input("Do you want to connect to the same device: ")
+        if sameDevice == "yes":
+             secondLine = raw_input("Please enter the acl you would like to edit: ")
+             SecondIp_ver = determineIPVersion(secondLine)
+             SecondDirection = determineDirection(secondLine)
+             preCheck(child, secondLine, SecondIp_ver, SecondDirection)
+             removeAndReplaceLine(child, secondLine, SecondIp_ver)
+             preCheck(child, secondLine, SecondIp_ver, SecondDirection)
+        else:
+            main()
+    sys.exit("Bye!")
 
 main()
