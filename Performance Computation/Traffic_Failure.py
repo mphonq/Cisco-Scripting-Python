8ffa7e0891ff010cdc5f12e50db41a5c4c47d1a1
mpho <mpho.nqache@gmail.com>
2016-08-15 00:22:07 +0100

    Calculate outage method to be completed

--- Performance Computation/Traffic_Failure.py
+++ Performance Computation/Traffic_Failure.py
@@ -2,8 +2,9 @@ import csv
 import sys
 import re
 
-
-
+row = 0 #The desired row to fetch from the CSV
+column = 7 #The desired column to feth from the CSV
+step = 1 #Used to either increment the column or the row
 
 def readCSV():
     spirentData = open('mlacp_switchover.csv')
@@ -21,7 +22,7 @@ def get_Dropped_Count():
     data = readCSV()
     values = []
     for row in data:
-        dropped_count = row[7]
+        dropped_count = row[column]
         values.append(dropped_count)
     return values
 
@@ -30,7 +31,7 @@ def get_Tx_Rate():
     data = readCSV()
     values = []
     for row in data:
-        tx_rate = row[8]
+        tx_rate = row[column + step]
         values.append(tx_rate)
     return values
 
@@ -39,13 +40,27 @@ def calculate_Loss(dropped_count, tx_rate):
 
         #maximum_Outage = raw_input("Please enter the maximum outage")
         passed_Streams = []
-        counter = 0
+        drop_value = 0
+        rate_value = 0
+        drop_counter = 0
+        tx_counter = 0
         total_number_of_drops = dropped_count.__len__()
         total_number_of_tx_rate = tx_rate.__len__()
-        for row in range(counter, total_number_of_drops):
-            print dropped_count[row]
-            counter += 1
+        for rown in range(drop_counter, total_number_of_drops - 1):
+            if drop_counter == 0:
+                header = getHeader(row, column)
+                #print header
+            drop_counter += 1
+            drop_value = float(dropped_count[drop_counter])
 
+        for rowne in range(tx_counter, total_number_of_tx_rate - 1):
+            if tx_counter == 0:
+                header = getHeader(row, column + step)
+                #print header
+            tx_counter += 1
+            rate_value = float(tx_rate[tx_counter])
+        total_loss = rate_value/drop_value
+        print total_loss
         #if value <= maximum_Outage:
             #passed_Streams.append(value)
 
@@ -54,9 +69,7 @@ def calculate_Loss(dropped_count, tx_rate):
 
 
 def main():
-    row = 0
-    column = 7
-    getHeader(row, column)
+
     dropped_count = get_Dropped_Count()
     tx_rate = get_Tx_Rate()
     calculate_Loss(dropped_count, tx_rate)
