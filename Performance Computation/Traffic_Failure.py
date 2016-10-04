65a04dabaf54775c8ba0b92a508a18ea4135404a
mphonq <mphonq@users.noreply.github.com>
2016-10-04 15:55:52 +0200

    Update Traffic_Failure.py
    Updated the ability to search for columns

--- Performance Computation/Traffic_Failure.py
+++ Performance Computation/Traffic_Failure.py
@@ -6,22 +6,38 @@ from matplotlib.ticker import MaxNLocator
 from collections import namedtuple
 import re
 
-row = 0  # The desired row to fetch from the CSV
-column = 5  # The desired column to fetch from the CSV
-step = 3  # Used to either increment the column or the row
-
-headers = []
+headers = []  # Contains whatever header you so choose to insert
 
 
 def readCSV():
     try:
-
         spirentData = open('mlacpswitchover.csv')
         readData = list(csv.reader(spirentData))
     except:
         sys.exit("file not found!!!")
 
     return readData
+
+
+def get_Dropped_Column():
+    data = readCSV()
+    headers = data[0]
+    index = 0
+    for s in headers:
+        if s == "Dropped Count (Frames)":
+            index = data[0].index(str(s))
+
+    return index
+
+
+def get_FPS_Column():
+    data = readCSV()
+    headers = data[0]
+    index = 0
+    for s in headers:
+        if s == "Frames per Second (fps)":
+            index = data[0].index(str(s))
+    return index
 
 
 def getHeader(row, column):
@@ -31,41 +47,39 @@ def getHeader(row, column):
     return header
 
 
-def get_Dropped_Count():
+def get_Dropped_Count(column):
     data = readCSV()
     values = []
     for row in data:
-
         dropped_count = row[column]
         values.append(dropped_count)
         headers.append(row[0])
     return values
 
 
-def get_Tx_Rate():
+def get_Tx_Rate(column):
     data = readCSV()
     values = []
     for row in data:
-        tx_rate = row[column + step]
+        tx_rate = row[column]
         values.append(tx_rate)
     return values
 
 
-def calculate_Loss():
+def calculate_Loss(drop_column, rate_column):
     loss = []
-    for drop_counter in range(0, get_Dropped_Count().__len__()):
+    for drop_counter in range(0, get_Dropped_Count(drop_column).__len__()):
         if drop_counter == 0:
             drop_counter += 1
         else:
-
-            drop_value = float("".join(get_Dropped_Count()[drop_counter].split(",")))
-            rate_value = float("".join(get_Tx_Rate()[drop_counter].split(",")))
+            drop_value = float("".join(get_Dropped_Count(drop_column)[drop_counter].split(",")))
+            rate_value = float("".join(get_Tx_Rate(rate_column)[drop_counter].split(",")))
             total_loss = drop_value / rate_value
             loss.append(total_loss)
     return loss
 
 
-def outage():
+def outage(dropped_column, rate_column):
     maximum_Outage = raw_input("Please enter the maximum outage in seconds: ")
     stream_selection = raw_input("do you want to see passed or failed streams: ")
     passed_Streams = []
@@ -73,7 +87,7 @@ def outage():
     passed_Headers = []
     failed_headers = []
     header_counter = 0
-    for loss_values in calculate_Loss():
+    for loss_values in calculate_Loss(dropped_column, rate_column):
         header_counter += 1
         if loss_values <= int(maximum_Outage):
             passed_Streams.append(loss_values)
@@ -97,9 +111,10 @@ def outage():
         sys.exit("Sorry but your input is not recognised, please enter passed or failed")
 
 
-
 def main():
-    outage()
+    dropped_column = get_Dropped_Column()
+    rate_column = get_FPS_Column()
+    outage(dropped_column, rate_column)
 
 
 main()
