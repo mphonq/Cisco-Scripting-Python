1beac09c28bfd678120cea61f9aca09ffdd83b37
mphonq <mphonq@users.noreply.github.com>
2016-10-04 10:35:40 +0200

    Update Traffic_Failure.py
    Added printing of streamblock names

--- Performance Computation/Traffic_Failure.py
+++ Performance Computation/Traffic_Failure.py
@@ -15,7 +15,8 @@ headers = []
 
 def readCSV():
     try:
-        spirentData = open('mlacp switchover.csv')
+
+        spirentData = open('mlacpswitchover.csv')
         readData = list(csv.reader(spirentData))
     except:
         sys.exit("file not found!!!")
@@ -23,7 +24,7 @@ def readCSV():
     return readData
 
 
-def getHeader():
+def getHeader(row, column):
     data = readCSV()
     computed_Data = list(data)
     header = computed_Data[row][column]
@@ -33,13 +34,12 @@ def getHeader():
 def get_Dropped_Count():
     data = readCSV()
     values = []
-
     for row in data:
-        str(row)[column]
-        #b = re.search(r'Dropped Count (Frames)', row)
+
         dropped_count = row[column]
         values.append(dropped_count)
-        print row
+        headers.append(row[0])
+    return values
 
 
 def get_Tx_Rate():
@@ -70,24 +70,36 @@ def outage():
     stream_selection = raw_input("do you want to see passed or failed streams: ")
     passed_Streams = []
     failed_Streams = []
-
+    passed_Headers = []
+    failed_headers = []
+    header_counter = 0
     for loss_values in calculate_Loss():
+        header_counter += 1
         if loss_values <= int(maximum_Outage):
             passed_Streams.append(loss_values)
+            passed_Headers.append(headers[header_counter])
         elif loss_values > int(maximum_Outage):
             failed_Streams.append(loss_values)
+            failed_headers.append(headers[header_counter])
+
     if stream_selection == 'passed':
+        pass_counter = 0
         for passed in passed_Streams:
-            print "%.3f" % passed
-    else:
+            print passed_Headers[pass_counter] + " %.3f " % passed
+            pass_counter += 1
+
+    elif stream_selection == 'failed':
+        fail_counter = 0
         for failed in failed_Streams:
-            print "%.3f" % failed
+            print failed_headers[fail_counter] + " %.3f" % failed
+            fail_counter += 1
+    else:
+        sys.exit("Sorry but your input is not recognised, please enter passed or failed")
 
 
 
 def main():
-    #outage()
-    get_Dropped_Count()
+    outage()
 
 
 main()
